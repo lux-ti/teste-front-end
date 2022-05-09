f1ec39a80a9e4f3671910b6038bbbcd15c198825
Wisterleys <wister.sousa2@gmail.com>
2022-05-09 08:45:50 -0300

    estrutura básica montada

---
+++ src/views/Teste.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <HelloWorld />
+    </div>
+</template>
+<script>
+import HelloWorld from '../components/HelloWorld.vue'
+export default {
+    name:'teste',
+    data(){
+        return {
+            nome:null
+        }
+    },
+    components:{
+        HelloWorld
+    }
+}
+</script>
