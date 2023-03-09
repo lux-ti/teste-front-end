f814579452507d0fc6f773a7441665d8f23586da
Bianca <you@example.com>
2023-03-09 16:27:03 -0300

    adds NotFound page layout

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -39,7 +39,6 @@
 
 <style scoped>
   #home {
-    padding: 10%;
     display: block;
     text-align: center;
   }
--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,12 +1,22 @@
 <template>
-  <div>Página não encontrada</div>
+  <section>
+    <img alt="github logo" src="../assets/vector.png" class="logo">
+    <strong>
+      <p>Desculpe!</p>
+      <p>404</p>
+      <p>Página não encontrada!</p>
+    </strong>
+  </section>
 </template>
  
 <script>
 
 </script>
  
-<style>
-
+<style scoped>
+  section {
+    text-align: center;
+    font-size: 32px;
+  }
 </style>
  
