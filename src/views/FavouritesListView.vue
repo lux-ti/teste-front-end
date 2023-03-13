9ad79722dbbc128b81f5c770c3a28e33e5d237cd
João Gabriel <bielgolin@gmail.com>
2023-03-12 23:49:38 -0300

    Favourites repositories list

---
+++ src/views/FavouritesListView.vue
@@ -0,0 +1,39 @@
+<template>
+    <TopBarComponent/>
+    <div class="header">
+        <img src="../assets/github.svg" alt="">
+        <p>Repositórios favoritos</p>
+    </div>
+    <ListRepositories :repositories="this.$store.state.likedRepositories"/>
+    
+    
+</template>
+
+<script setup>
+import TopBarComponent from '../components/TopBarComponent.vue';
+import ListRepositories from '../components/ListRepositories.vue';
+</script>
+
+<style scoped>
+    * {
+        font-family: 'Inter', sans-serif;
+    }
+
+    .header {
+        display: flex;
+        margin-top: 15px;
+        margin-left: 30px;
+        margin-bottom: 42px;
+    }
+
+    .header p {
+        font-size: 45px;
+        margin: 0;
+        font-family: 'Roboto Mono', monospace;
+    }
+
+    .header img{
+        width: 50px;
+        height: 50px;
+    }
+</style>
